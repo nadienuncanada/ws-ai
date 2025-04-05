b1befa927b4d789e142b8e1499675d45e6c3b797
Nack34 <gmnack9@gmail.com>
2025-04-05 19:34:31 -0300

    Init Scrapy

---
+++ ws_spider/spiders/example_spider.py
@@ -0,0 +1,12 @@
+import scrapy
+
+class ExampleSpider(scrapy.Spider):
+    name = "example"
+    start_urls = ['https://quotes.toscrape.com/']
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+            }
